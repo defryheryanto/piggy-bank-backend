8defc9eb911a0bda9f22fd371ea073403a3a4a48
defry256 <defry.heryanto@hotmail.com>
2022-08-16 16:23:47 +0700

    add user database migrations

---
+++ db/migrate.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"database/sql"
+	"flag"
+	"fmt"
+	"log"
+
+	"github.com/defryheryanto/piggy-bank-backend/config"
+	_ "github.com/defryheryanto/piggy-bank-backend/config/env"
+	_ "github.com/golang-migrate/migrate/source"
+	"github.com/golang-migrate/migrate/v4"
+	"github.com/golang-migrate/migrate/v4/database/postgres"
+	"github.com/golang-migrate/migrate/v4/source/file"
+	_ "github.com/mattes/migrate/source/file"
+)
+
+func main() {
+	downFlag := flag.Bool("down", false, "database migration down")
+	flag.Parse()
+	fmt.Println("Opening piggy bank database connection...")
+	db, err := sql.Open(
+		"postgres",
+		fmt.Sprintf(
+			"postgres://%s:%s@%s:%s/%s?sslmode=%s",
+			config.PiggyBankDBUsername(),
+			config.PiggyBankDBPassword(),
+			config.PiggyBankDBHost(),
+			config.PiggyBankDBPort(),
+			config.PiggyBankDBName(),
+			config.PiggyBankDBSSLMode(),
+		),
+	)
+	fmt.Println("Piggy bank database connected.")
+	if err != nil {
+		log.Fatalf("error opening migration database %v", err)
+	}
+
+	fmt.Println("Generating postgres instance...")
+	driver, err := postgres.WithInstance(db, &postgres.Config{})
+	if err != nil {
+		log.Fatalf("error generating postgres instance %v", err)
+	}
+	fmt.Println("Postgres instance generated.")
+
+	fmt.Println("Opening migration files...")
+	fsrc, err := (&file.File{}).Open("file://db/migrations")
+	if err != nil {
+		log.Fatalf("error opening migration files %v", err)
+	}
+	fmt.Println("Migration files opened.")
+
+	fmt.Println("Creating migration instance...")
+	m, err := migrate.NewWithInstance("file", fsrc, "postgres", driver)
+	if err != nil {
+		log.Fatalf("error generating migrate instance %v", err)
+	}
+	fmt.Println("Migration instance created.")
+
+	if *downFlag {
+		fmt.Println("Rollback migration..")
+		if err := m.Down(); err != nil && err != migrate.ErrNoChange {
+			log.Fatal(err)
+		}
+		fmt.Println("Rollback complete.")
+	} else {
+		fmt.Println("Migrating migration..")
+		if err := m.Up(); err != nil && err != migrate.ErrNoChange {
+			log.Fatal(err)
+		}
+		fmt.Println("Migrate complete.")
+	}
+}
--- go.mod
+++ go.mod
@@ -10,6 +10,15 @@ require (
 )
 
 require (
+	github.com/hashicorp/errwrap v1.1.0 // indirect
+	github.com/hashicorp/go-multierror v1.1.1 // indirect
+	github.com/lib/pq v1.10.2 // indirect
+	go.uber.org/atomic v1.7.0 // indirect
+)
+
+require (
+	github.com/golang-migrate/migrate v3.5.4+incompatible
+	github.com/golang-migrate/migrate/v4 v4.15.2
 	github.com/jackc/chunkreader/v2 v2.0.1 // indirect
 	github.com/jackc/pgconn v1.13.0 // indirect
 	github.com/jackc/pgio v1.0.0 // indirect
@@ -20,6 +29,7 @@ require (
 	github.com/jackc/pgx/v4 v4.17.0 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
+	github.com/mattes/migrate v3.0.1+incompatible
 	golang.org/x/crypto v0.0.0-20220722155217-630584e8d5aa // indirect
 	golang.org/x/text v0.3.7 // indirect
 )
