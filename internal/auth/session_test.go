fc3979c6d1227a5a0e340f95464a9e9eaaf7e1aa
defryheryanto <defry.heryanto@hotmail.com>
2022-09-08 00:47:34 +0700

    Add More Session Test

--- internal/auth/session_test.go
+++ internal/auth/session_test.go
@@ -10,14 +10,28 @@ import (
 
 func TestAuthFromContext(t *testing.T) {
 	ctx := context.TODO()
-	currentSession := &auth.AuthSession{
-		UserID:   1,
-		Username: "defryheryanto",
-	}
-	ctx = context.WithValue(ctx, auth.SessionKey, currentSession)
 
-	session := auth.FromContext(ctx)
-	assert.NotNil(t, session)
-	assert.Equal(t, currentSession.UserID, session.UserID)
-	assert.Equal(t, currentSession.Username, session.Username)
+	t.Run("should get current session", func(t *testing.T) {
+		currentSession := &auth.AuthSession{
+			UserID:   1,
+			Username: "defryheryanto",
+		}
+		ctx = context.WithValue(ctx, auth.SessionKey, currentSession)
+
+		session := auth.FromContext(ctx)
+		assert.NotNil(t, session)
+		assert.Equal(t, currentSession.UserID, session.UserID)
+		assert.Equal(t, currentSession.Username, session.Username)
+	})
+
+	t.Run("return nil if current session invalid", func(t *testing.T) {
+		currentSession := &auth.User{
+			UserID:   1,
+			Username: "defryheryanto",
+		}
+		ctx = context.WithValue(ctx, auth.SessionKey, currentSession)
+
+		session := auth.FromContext(ctx)
+		assert.Nil(t, session)
+	})
 }
